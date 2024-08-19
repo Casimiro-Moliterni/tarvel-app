d7288084f6fd7ad8ae0c2d33a66b2eec53bdbbfa
Casimiro-Moliterni <moliternicasimiro@hotmail.it>
2024-08-19 18:46:18 +0200

    fix css

--- resources/views/components/accordionStops.blade.php
+++ resources/views/components/accordionStops.blade.php
@@ -19,21 +19,23 @@
 </div>
 
 @foreach ($event as $events)
-<div class="modal fade" id="editStopModal-{{ $event->id }}" tabindex="-1" role="dialog" aria-labelledby="editStopModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="editStopModalLabel">Modifica Tappa</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                @include('admin.stops.edit', ['stop' => $event])
+<section id="section-modal">
+    <div class="modal fade my-bg-modal" id="editStopModal-{{ $event->id }}" tabindex="-1" role="dialog" aria-labelledby="editStopModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editStopModalLabel">Modifica Tappa</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    @include('admin.stops.edit', ['stop' => $event])
+                </div>
             </div>
         </div>
     </div>
-</div>
+</section>
 @endforeach
 
 

8e3193f6ec540e2d9c4a4493bf9ae37a4a71f399
anto-iorfida <antonioiorfida@hotmail.com>
2024-08-20 17:20:23 +0200

    kkk

--- resources/views/components/ContentStopShow.blade.php
+++ resources/views/components/ContentStopShow.blade.php
@@ -2,15 +2,20 @@
 <h2>show sstop</h2>
 
 @foreach ($event->ratings as $rating)
-    {{-- @dump($rating->rating) --}}
+    @if (empty($rating->rating))
+        <div class="d-inline-block">
+            <x-ratingComponent :trip="$event->id_trip" :stop="$event->id" />
+        </div>
+    @else{
+        <div class="d-inline-block">
+            {{-- <x-ratingCard :event="$event" /> --}}
+        </div>
+
+        }
+    @endif
 @endforeach
-<div class="d-inline-block">
-    <x-ratingComponent :trip="$event->id_trip" :stop="$event->id" />
-</div>
-<div class="d-inline-block">
-    <x-ratingCard :event="$event" />
-</div>
- 
+
+
 
 @push('scripts')
     @vite(['resources/js/app.js'])

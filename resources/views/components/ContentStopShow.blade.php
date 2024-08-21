5b3fa6ca7c4e239183af071d0fbdb3acbe4704ae
anto-iorfida <antonioiorfida@hotmail.com>
2024-08-21 11:30:51 +0200

    pusch

--- resources/views/components/ContentStopShow.blade.php
+++ resources/views/components/ContentStopShow.blade.php
@@ -1,26 +1,8 @@
-{{-- @props(['event'])
-<h2>show sstop</h2>
-
-<div class="d-inline-block">
-    <x-ratingComponent :trip="$event->id_trip" :stop="$event->id" />
-</div>
-<div class="d-inline-block" class="container-rating-star">
-
-    <x-ratingCard :event="$event" />
-</div> --}}
 
 @props(['event'])
 
 <div id="ratingComponentWrapper">
-    {{-- @if ($event->ratings->isNotEmpty()) --}}
-    
-        {{-- Se c'è almeno una valutazione, mostra il ratingCard --}}
-        <x-ratingCard :event="$event" :trip="$event->id_trip" :stop="$event->id"  />
-    {{-- @else --}}
-    
-        {{-- Se non ci sono valutazioni, mostra il ratingComponent --}}
-        {{-- <x-ratingComponent :trip="$event->id_trip" :stop="$event->id" /> --}}
-    {{-- @endif --}}
+    <x-ratingCard :event="$event" :trip="$event->id_trip" :stop="$event->id" />
 </div>
 
 @push('scripts')

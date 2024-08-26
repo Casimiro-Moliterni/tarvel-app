bcb239d0d4981be22a23e5d1265bb722f9cf78ca
Casimiro-Moliterni <moliternicasimiro@hotmail.it>
2024-08-26 16:45:33 +0200

    fix style css note

--- resources/js/note.js
+++ resources/js/note.js
@@ -48,13 +48,12 @@ function initializeNoteHandlers() {
                         if (data.status === 'success') {
                             // Crea un nuovo elemento per la nuova nota
                             const noteElement = document.createElement('div');
-                            noteElement.className = 'my-col alert alert-info d-flex justify-content-between px-5';
+                            noteElement.className = 'my-col d-flex flex-column align-items-center';
                             noteElement.setAttribute('data-note-id', data.note.id);
                             // noteElement.textContent = data.note.text; // Imposta il testo della nota
                             noteElement.innerHTML = `
-                            <p class="mb-0">${data.note.text}</p>
-                            <div class="d-flex gap-5">
-                                <i class="fa-regular fa-pen-to-square"></i>
+                            <p class="mb-0 flex-grow-1">${data.note.text}</p>
+                            <div class="d-flex gap-5 mb-3">
                                 <i class="fa-solid fa-delete-left"></i>
                             </div>
                         `;
@@ -77,7 +76,7 @@ function initializeNoteHandlers() {
         // Funzione per collegare l'evento click (// eliminazione nota)
         function attachDeleteListener(icon) {
             icon.addEventListener('click', function () {
-                const noteElement = this.closest('.alert');
+                const noteElement = this.closest('.my-col');
                 const noteId = noteElement.getAttribute('data-note-id');
     
                 axios.delete(`/admin/notes/${noteId}`, {
--- resources/views/components/noteCard.blade.php
+++ resources/views/components/noteCard.blade.php
@@ -1,9 +1,9 @@
 @props(['event'])
 <div id="my-note">
     <div class="notes-container container mt-4 text-center row">
-        <div class="col">
-            <div class="my-col">
-                <h4 class="mb-4">Crea una nuova nota</h4>
+        <div class="col ">
+            <div class="col-6 my-form-note mb-4 m-auto">
+                <h4 class="mb-4">NEW NOTE</h4>
                 <div class="row d-flex flex-column">
                     <div class="col">
                         <form id="form-notes" action="{{ route('admin.notes.store') }}" class="text-center" method="POST">
@@ -14,22 +14,21 @@
                                 <div class="error text-danger fs-4"></div>
                             </div>
                             <input type="hidden" name="id_stop" value="{{ $event->id }}">
-                            <button type="submit" class="btn btn-primary my-4">Crea Nota</button>
+                            <button type="submit" class="btn my-4">Crea Nota</button>
                         </form>
                     </div>
                 </div>
             </div>
             {{-- note create  --}}
             <div id="notesWrapper" class="">
-                <div class=" my-wrap-note">
+                <div class=" my-wrap-note  w-75 m-auto">
                     @foreach ($event->notes as $note)
-                        <div class="my-col alert alert-info d-flex justify-content-between px-5"
+                        <div class="my-col d-flex flex-column align-items-center"
                             data-note-id="{{ $note->id }}">
-                            <p class="mb-0">
+                            <p class="mb-0 flex-grow-1">
                                 {{ $note->text }}
                             </p>
-                            <div class="d-flex gap-5">
-                                <i class="fa-regular fa-pen-to-square"></i>
+                            <div class="d-flex gap-5 mb-3">
                                 <i class="fa-solid fa-delete-left"></i>
                             </div>
                         </div>

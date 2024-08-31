b65dc2f1a0141b4fbd761557790a48675b16b15c
Mattia Giarrusso <giarrusso.m@yahoo.com>
2024-08-31 12:08:35 +0200

    Feat: fix home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,32 +1,38 @@
 @extends('layouts.app')
 @section('content')
     <section id="welcome-page">
+        <header class="row px-5">
+            <div class="ms-logo-home">
+                <img src="{{ asset('img/logo-black.png') }}" alt="Trip Notes">
+            </div>
+        </header>
+
         <div class="background">
             <div class="shape"></div>
             <div class="shape"></div>
         </div>
         <form>
-            <h3>Home Page</h3>
+            <h3 class="text-black">Home Page</h3>
             <div class="container">
 
                 <a href="#" class="button type--A">
                     <div class="button__line"></div>
                     <div class="button__line"></div>
-                    <span class="button__text">COME FUNZIONA</span>
+                    <span class="button__text text-dark">COME FUNZIONA</span>
                     <div class="button__drow1"></div>
                     <div class="button__drow2"></div>
                 </a>
                 <a href="{{ route('login') }}" class="button type--B">
                     <div class="button__line"></div>
                     <div class="button__line"></div>
-                    <span class="button__text">LOGIN</span>
+                    <span class="button__text text-dark">LOGIN</span>
                     <div class="button__drow1"></div>
                     <div class="button__drow2"></div>
                 </a>
                 <a href="{{ route('register') }}" class="button type--C">
                     <div class="button__line"></div>
                     <div class="button__line"></div>
-                    <span class="button__text">REGISTRAZIONE</span>
+                    <span class="button__text text-dark">REGISTRAZIONE</span>
                     <div class="button__drow1"></div>
                     <div class="button__drow2"></div>
                 </a>

977ecec8d0d06d22a07759c2ef36b37ee814eae2
Casimiro-Moliterni <moliternicasimiro@hotmail.it>
2024-09-01 12:54:33 +0200

    feature add image

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -44,76 +44,48 @@
                 {{-- /logo della sidebar --}}
 
                     <li class="{{ Route::currentRouteName() == 'admin.dashboard' ? 'hovered curvet curveb' : '' }}" id="dashboard-link"
-                    style="{{ Route::currentRouteName() == 'admin.dashboard' ? 'background-color: white;color: black;' : '' }}" >
+                    style="{{ Route::currentRouteName() == 'admin.dashboard' ? 'background-color: #E3F2FD;color: black;' : '' }}" >
                         <a href="{{ route('admin.dashboard') }}" class="icon fs-5" 
                         style="{{ Route::currentRouteName() == 'admin.dashboard' ? 'color: black;' : '' }}" >
                             <i class="fa-solid fa-house-user"></i><span class="ms-span">Dashboard</span>
                         </a>
                     </li>
                     <li class="{{ Route::currentRouteName() == 'admin.trips.index' ? 'hovered curvet curveb' : '' }}"  id="trip-index-link" 
-                    style="{{ Route::currentRouteName() == 'admin.trips.index' ? 'background-color: white;color: black;' : '' }}">
+                    style="{{ Route::currentRouteName() == 'admin.trips.index' ? 'background-color: #E3F2FD;color: black;' : '' }}">
                         <a href="{{ route('admin.trips.index') }}" class="icon fs-5"
                         style="{{ Route::currentRouteName() == 'admin.trips.index' ? 'color: black;' : '' }}">
                             <i class="fa-solid fa-plane"></i><span class="ms-span">Viaggi</span>
                         </a>
                     </li>
                     <li class="{{ Route::currentRouteName() == 'admin.trips.create' ? 'hovered curvet curveb' : '' }}"
-                    style="{{ Route::currentRouteName() == 'admin.trips.create' ? 'background-color: white;color: black;' : '' }}">
+                    style="{{ Route::currentRouteName() == 'admin.trips.create' ? 'background-color: #E3F2FD;color: black;' : '' }}">
                         <a href="{{ route('admin.trips.create') }}" class="icon fs-5"
                         style="{{ Route::currentRouteName() == 'admin.trips.create' ? 'color: black;' : '' }}">
                             <i class="fa-solid fa-circle-plus"></i><span class="ms-span">Aggiungi viaggio</span>
                         </a>
                     </li>
                     <li class="{{ Route::currentRouteName() == 'admin.garbage' ? 'hovered curvet curveb' : '' }}"
-                    style="{{ Route::currentRouteName() == 'admin.garbage' ? 'background-color: white;color: black;' : '' }}">
+                    style="{{ Route::currentRouteName() == 'admin.garbage' ? 'background-color: #E3F2FD;color: black;' : '' }}">
                         <a href="{{ route('admin.garbage') }}" class="icon fs-5"
                         style="{{ Route::currentRouteName() == 'admin.garbage' ? 'color: black;' : '' }}">
                             <i class="fa-solid fa-trash-can"></i><span class="ms-span">Cestino</span>
                         </a>
                     </li>
                     <li class="{{ Route::currentRouteName() == 'profile' ? 'hovered curvet curveb' : '' }}"
-                    style="{{ Route::currentRouteName() == 'profile' ? 'background-color: white;color: black;' : '' }}">
+                    style="{{ Route::currentRouteName() == 'profile' ? 'background-color: #E3F2FD;color: black;' : '' }}">
                         <a href="{{ route('profile.edit') }}" class="icon fs-5"
                         style="{{ Route::currentRouteName() == 'profile' ? 'color: black;' : '' }}">
                             <i class="fa-solid fa-user-gear"></i><span class="ms-span">Profilo</span>
                         </a>
                     </li>
                 </ul>
-            </div>
-                <li class="{{ Route::currentRouteName() == 'admin.dashboard' ? 'hovered' : '' }}" id="dashboard-link">
-                    <a href="{{ route('admin.dashboard') }}" class="icon fs-5">
-                        <i class="fa-solid fa-house-user"></i><span class="ms-span">Dashboard</span>
-                    </a>
-                </li>
-                <li class="{{ Route::currentRouteName() == 'admin.trips.index' ? 'hovered' : '' }}"
-                    id="trip-index-link">
-                    <a href="{{ route('admin.trips.index') }}" class="icon fs-5">
-                        <i class="fa-solid fa-plane"></i><span class="ms-span">Viaggi</span>
-                    </a>
-                </li>
-                <li class="{{ Route::currentRouteName() == 'admin.trips.create' ? 'hovered' : '' }}">
-                    <a href="{{ route('admin.trips.create') }}" class="icon fs-5">
-                        <i class="fa-solid fa-circle-plus"></i><span class="ms-span">Aggiungi viaggio</span>
-                    </a>
-                </li>
-                <li class="{{ Route::currentRouteName() == 'admin.garbage' ? 'hovered' : '' }}">
-                    <a href="{{ route('admin.garbage') }}" class="icon fs-5">
-                        <i class="fa-solid fa-trash-can"></i><span class="ms-span">Cestino</span>
-                    </a>
-                </li>
-                <li class="{{ Route::currentRouteName() == 'profile' ? 'hovered' : '' }}">
-                    <a href="{{ route('profile.edit') }}" class="icon fs-5">
-                        <i class="fa-solid fa-user-gear"></i><span class="ms-span">Profilo</span>
-                    </a>
-                </li>
             </ul>
         </div>
 
         <!-- ========================= Main ==================== -->
         <div class="main position-relative">{{-- questa classe qua --}}
             {{-- wave  --}}
-            <img class="wave position-absolute w-100" style="height: 300px ; z-index:0"
-                src="{{ asset('img/wave.png') }}">
+            <img class="wave position-absolute w-100" style="height: 300px ; z-index:0" src="{{ asset('img/wave.png') }}">
             <div class="topbar">
                 <div class="toggle">
                     {{-- <ion-icon name="menu-outline"></ion-icon> --}}

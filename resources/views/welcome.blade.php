c7bc4ef3734572a394dbfbd54fbcbef478642d90
Casimiro-Moliterni <moliternicasimiro@hotmail.it>
2024-09-02 09:24:24 +0200

    fix component for welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,55 +40,7 @@
                 </a>
             </div>
         </form>
-        <div class=" d-none z-3 position-relative  m-auto" id="content-page">
-            <i id="close-welcome" class="fa-regular fa-circle-xmark text-danger"></i>
-            <h2 class="text-center mb-4 pt-3">Gestisci il tuo viaggio con Trip Notes!</h2>
-            <div class="row px-4">
-                <div class="mini-card">
-                    <h4 class="text-center">COME FUNZIONA?</h4>
-                    <div class="mini-card-body p-3">
-                        Tramite quest'applicazione, ogni viaggiatore potrà organizzare il proprio viaggio nel modo migliore.
-                        Se sei un nuovo utente, completa lla registrazione nell'apposita sezione. Se invece se già iscritto,
-                        fai login con le tue credenziali.
-                    </div>
-                </div>
-                <div class="mini-card">
-                    <h4 class="text-center">LA TUA DASHBOARD?</h4>
-                    <div class="mini-card-body p-3">
-                        Dopo l'accesso/iscrizione, verrai indirizzato alla tua dashboard personale. Una volta lì potrai
-                        compiere diverse azioni:
-
-                        - creare viaggi specificando paese, città, copertina e durata del viaggio e altro;
-
-                        - consultare la lista dei viaggi creati cliccando su 'Viaggi' nel menù laterale;
-
-                        - modificare o cestinare viaggi creati, premendo rispettivamente sui pulsanti blu e rosso nella card
-                        di viaggio;
-
-                        - eliminare definitivamente i viaggi nella sezione 'Cestino', accessibile dal pulsante del menù
-                        laterale
-                        In basso, nell'ultima voce della dashboard, potrai accedere alla sezione 'Profilo', dove puoi cambiare tutte le informazioni relative al tuo account o eventualmente eliminare il tuo profilo.
-
-                    </div>
-                </div>
-                <div class="mini-card">
-                    <h4 class="text-center">LA TUE CARD?</h4>
-                    <div class="mini-card-body p-3">
-                        Cliccando sulla card di viaggio potrai accedere ai giorni del tuo itinerario, pari al numero di giorni di vacanza, al meteo e alla geolocalizzazione della città. 
-                    </div>
-                </div>
-                <div class="mini-card">
-                      <h4 class="text-center">LE TUE TAPPE?</h4>
-                      <div class="mini-card-body p-3">
-                        Aggiungi tappe ad ogni giornata del tuo viaggio per vivere sempre nuove esperienze. Nel form della tappa giornaliera potrai inserire città e via che intendi visitare quel giorno, cibo mangiato, curiosità ecc. Potrai anche modificare in caso di cambiamenti. 
-                        OCCHIO! Una tappa non può sovrapporsi ad un altra, quindi scegli con precisione gli orari di ogni tappa per una gestione ordinata della giornata.
-                      </div>
-                </div>
-            </div>
-            <footer class="text-center">
-                CHE ASPETTI! <span class="btn"><a href="{{route('register')  }}">ISCRIVITI</a></span> E AFFRONTA I TUOI VIAGGI NEL MODO MIGLIORE CON TRIP NOTES
-            </footer>
-        </div>
+        <x-welcomeContent />
     </section>
 @endsection
 <script>
